c85347e0ca8412ccddb381bce7cc74bfcd2cfd94
cartory <xXcartoryXx@gmail.com>
2021-08-03 18:06:44 -0400

    added stars component

---
+++ src/components/stars/stars.jsx
@@ -0,0 +1,11 @@
+import './stars.css'
+
+const Stars = () => {
+	return (
+		<div className="stars" style={{ height: 0 }}>
+			<div className="asteroidsb-belt"></div>
+		</div>
+	)
+}
+
+export default Stars
