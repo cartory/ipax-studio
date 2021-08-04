5ee725d461161647ab7b9efa8407685b768956e3
cartory <xXcartoryXx@gmail.com>
2021-08-04 17:23:17 -0400

    update hexagon

--- src/components/hexagon/hexagon.jsx
+++ src/components/hexagon/hexagon.jsx
@@ -1,23 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './hexagon.css'
 
 import PropTypes from 'prop-types'
 
-const Hexagon = ({ side = 100, icon, title }) => {
+const Hexagon = ({ side = 100, icon }) => {
     return (
-        <div
-            className="hexagon"
-            style={{ width: side, height: Math.sqrt(3) * side }}
-        >
-            <img src={icon} loading="lazy" alt="#" srcset={icon} />
-            <p>{title}</p>
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div
+                className="hexagon"
+                style={{ width: side, height: Math.sqrt(3) * side }}
+            >
+                <i className={icon}></i>
+            </div>
         </div>
     )
 }
 
 Hexagon.propTypes = {
     side: PropTypes.number,
-    src: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    // title: PropTypes.string.isRequired,
 }
 
 export default Hexagon
