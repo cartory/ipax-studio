884d1a2caff0ac4b4d3f8c4555c6ae537c3f7414
cartory <xXcartoryXx@gmail.com>
2021-08-21 06:02:33 -0400

    hexagon updated

--- src/components/hexagon/hexagon.jsx
+++ src/components/hexagon/hexagon.jsx
@@ -4,6 +4,7 @@ import './hexagon.css'
 import PropTypes from 'prop-types'
 
 const Hexagon = ({ side = 100, icon }) => {
+    let scale = navigator.appVersion.includes('Safari') ? 5 : 1
     return (
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div
@@ -17,7 +18,9 @@ const Hexagon = ({ side = 100, icon }) => {
                 <img
                     src={require(`../../assets/${icon}.svg`).default}
                     alt="#"
-                    style={{ transform: 'scale(5) translateY(-5.5%)' }} />
+                    style={{
+                        transform: `scale(${scale}) translateY(-5.5%)`
+                    }} />
             </div>
         </div>
     )
